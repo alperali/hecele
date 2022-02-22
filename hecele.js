941c4751d05704021141f55732b588a15219afc1
A. Alper Atıcı <alper.goplay@gmail.com>
2022-02-22 11:12:11 +0300

    demo site

---
+++ hecele.js
@@ -0,0 +1,32 @@
+const SHY = '\u00AD';  // soft hyphen
+const HRD = '\u2010';  // hard hyphen
+const kurallar = [
+  { ptn: /^[aeiouöüıİ][bcçdfgğhjklmnprsştvyz][aeiouöüıİ]/i,    len: 1}, // 2.
+  { ptn: /^[bcçdfgğhjklmnprsştvyz][aeiouöüıİ]{2}/i,            len: 2}, // 3.
+  { ptn: /^[aeiouöüıİ][bcçdfgğhjklmnprsştvyz]{2}[aeiouöüıİ]/i, len: 2}, // 4.
+  { ptn: /^([bcçdfgğhjklmnprsştvyz][aeiouöüıİ]){2}/i,          len: 2}, // 5.
+  { ptn: /^[aeiouöüıİ][bcçdfgğhjklmnprsştvyz]{2}($|[bcçdfgğhjklmnprsştvyz])/i, len: 3}, // 6.
+  { ptn: /^[bcçdfgğhjklmnprsştvyz][aeiouöüıİ][bcçdfgğhjklmnprsştvyz]($|[bcçdfgğhjklmnprsştvyz][aeiouöüıİ])/i, len: 3},  // 7.
+  { ptn: /^[bcçdfgğhjklmnprsştvyz][aeiouöüıİ][bcçdfgğhjklmnprsştvyz]{2}($|[bcçdfgğhjklmnprsştvyz])/i, len: 4},          // 8.
+  { ptn: /^[bcçdfgğhjklmnprsştvyz]{2}[aeiouöüıİ][bcçdfgğhjklmnprsştvyz]/i, len: 4}       // 9.
+];
+
+function hecele(szck, tire = SHY)
+{
+  if (szck.length < 3)        // 1.
+    return szck;
+
+  for (const kural of kurallar)
+    if (kural.ptn.test(szck))
+      if (szck.length > kural.len)
+        return `${szck.slice(0, kural.len)}${tire}${hecele(szck.slice(kural.len), tire)}`;
+      else
+        return szck;
+    
+  return szck;   // hiçbiri uymadı, aynen döndür.
+}
+
+Object.defineProperty(hecele, 'SHY', { value: SHY});
+Object.defineProperty(hecele, 'HRD', { value: HRD});
+
+export default hecele;
